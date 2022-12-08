c8d08032b5fc2d753035f3f5960619f9cc00c234
Dynesshely <catrol@qq.com>
2022-12-08 20:17:48 +0800

    📝 Chore: Init Project

---
+++ VideoCompressorUI/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace VideoCompressorUI.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
---
+++ VideoCompressorUI/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace VideoCompressorUI.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
